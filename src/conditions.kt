4b4ce48cf91b4da3d03489607501b24143761723
Shourya Singh <shourya124sensei@gmail.com>
2026-01-08 00:43:27 +0530

    conditions practice

--- src/conditions.kt
+++ src/conditions.kt
@@ -5,5 +5,8 @@ fun main()
     if(a>b) println("true")
     else println("false")
 
+    //There is no ternary operator in kotlin, but we can use if inline in
+    // statements as an expressions
+    println(if(a>b) a else b)
 
 }
