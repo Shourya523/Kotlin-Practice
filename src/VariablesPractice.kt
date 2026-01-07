17793fcacc0af1c18e628e3cc62810aaa5937212
Shourya Singh <shourya124sensei@gmail.com>
2026-01-08 00:35:24 +0530

    conditions practice

--- src/VariablesPractice.kt
+++ src/VariablesPractice.kt
@@ -1,4 +1,26 @@
-fun main()
+fun main(args:Array<String>)
 {
+    var a :Int = 14
+    println(a)
+    a=34
+    a+=3
+    println(a)
+    var c: Int=36
+    println(c)
+    val b: Boolean =true
+    println(b)
+
+    // String interpolation
+    println("value of a is: $a")
+    println("value of c is: $c")
+
+    //Operators:arithmetic
+    println("Value of a+c =${a+c}")
+    println("Value of a-c =${a-c}")
+    println("Value of a*c=${a*c}")
+    println("Value of a/c = ${a/c.toFloat()}")
+    println("Value of a%c =${a%c}")
+
+
 
 }
---
+++ src/conditions.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    var a=45
+    var b=35
+    if(a>b) println("true")
+    else println("false")
+
+
+}
